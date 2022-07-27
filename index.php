2444c506d0e5e2650a00d64f1fca83c413df8d2f
henrisusanto <henri@member.id>
2022-07-28 04:02:29 +0700

    done

---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YoutoMP3: YoutoMP3 Free MP3 Downloads</title>
+    <meta name="keywords" content="YoutoMP3,YoutoMP3,YoutoMP3,YoutoMP3">
+    <meta name="description" content="YoutoMP3 allows you to search for and download your favorite music or videos for free from a variety of internet sources. No registration is required to use YoutoMP3." />
+    <meta property="og:title" content="YoutoMP3: YoutoMP3 Free MP3 Downloads - YoutoMP3">
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://youtomp3s.io/" />
+    <meta property="og:image" content="/theme/youtomp3/images/pic.png" />
+    <meta name="theme-color" content="#149c14">
+    <meta name="msapplication-TileColor" content="#1676c2">
+    <meta name="theme-color" content="#1676c2">
+    <meta name="msapplication-navbutton-color" content="#1676c2">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1676c2">
+    <link rel="canonical" href="https://youtomp3s.io/" />
+    <link href="/theme/youtomp3/images/favicon.ico" rel="icon" type="image/x-icon">
+    <link rel="stylesheet" href="<?= get_theme_file_uri('css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= get_theme_file_uri('css/bootstrap-theme.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= get_theme_file_uri('css/font-awesome.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= get_theme_file_uri('css/main.css') ?>">
+</head>
+
+<body style="background: #1676c2;">
+    <div id="nav">
+        <a href="<?= site_url() ?>" title="YoutoMP3">Home</a>
+        <a href="#use" title="YoutoMP3 FAQ">FAQ</a>
+    </div>
+    <div id="content">
+        <a href="<?= site_url() ?>">
+            <img id="logo" src="<?= get_theme_file_uri('images/logo.png') ?>" alt="YoutoMP3" title="YoutoMP3">
+        </a>
+        <style type="text/css">
+            #downloader_container input[type=text],
+            #downloader_container select,
+            #downloader_container textarea {
+                color: black;
+                width: 100%;
+                /* Full width */
+                padding: 12px;
+                /* Some padding */
+                border: 1px solid #ccc;
+                /* Gray border */
+                border-radius: 4px;
+                /* Rounded borders */
+                box-sizing: border-box;
+                /* Make sure that padding and width stays in place */
+                margin-top: 6px;
+                /* Add a top margin */
+                margin-bottom: 16px;
+                /* Bottom margin */
+                resize: vertical
+                    /* Allow the user to vertically resize the textarea (not horizontally) */
+            }
+
+            /* Style the submit button with a specific background color etc */
+            #downloader_container button {
+                background-color: #64c896;
+                color: white;
+                padding: 12px 20px;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+            }
+
+            /* Add a background color and some padding around the form */
+            #downloader_container {
+                border-radius: 5px;
+                background-color: #1676c2;
+                padding: 20px;
+            }
+        </style>
+        <div id="downloader_container">
+            <input type="text" id="query">
+            <button onclick="download('mp3')">MP3</button>
+            <button onclick="download('mp4a')">144</button>
+            <button onclick="download('360')">360</button>
+            <button onclick="download('480')">480</button>
+            <button onclick="download('720')">720</button>
+            <button onclick="download('1080')">1080</button>
+            <button onclick="download('4k')">4K</button>
+            <button onclick="download('8k')">8K</button>
+        </div>
+        <div id="form_text">
+            By click 'Search Button' you accept our Terms of Use.<br><br> <a href="https://near-me.store/en/youtomp3" target="_blank" rel="nofollow" style="font-size: 14px;">-> download youtomp3 app <-< /a>
+                    <br><br>
+                    <a href="https://downvideo.quora-wiki.com/tiktok-video-downloader" target="_blank" rel="nofollow" style="font-size: 14px;">-&gt; All-in-one video downloader &lt;-</a>
+        </div>
+        <div id="load">
+            <i class="fa fa-spinner fa-spin"></i>
+        </div>
+        <div id="result_box" style="display: none">
+            <div class="frameme" style="height: 400px;margin-top: 30px;">
+            </div>
+        </div>
+        <div id="text">
+            <h1>YoutoMP3: YoutoMP3 Free MP3 Downloads</h1>
+            <p>
+                YoutoMP3 is a well-known and completely free mp3 music download site. YoutoMP3 is also a free search engine that searches the internet for MP3 music files. By typing your search query into the YoutoMP3 search box, you can download your favorite songs as mp3 music or videos, and the MP3 audio files will be found on the internet. A list of your mp3 music or video results will be displayed if any results match your search term. It's just so simple. </p>
+            <p>
+                YoutoMP3 is a music and free mp3 download search engine. YoutoMP3 allows you to search for and download music in up to 320 kbps quality to your local device. Music may be found all over the internet, including the most popular music streaming and mp3 download services. </p>
+            <p>
+                YoutoMP3 offers a variety of file formats, including MP3, MP4, and M4a. You may download Mp3, 64kbps, 128kbps, 256kbps, Mp4, 320p, 460p, 720p, and Mkv in all formats for free. Click Here To Visit YoutoMP3. Simply type your video into the search bar and wait milliseconds for a response. </p>
+            <p>
+                You may also copy and paste a URL into the search field before pressing the search button. The conversion process begins, and within seconds, a downloaded audio file is available for download. This YoutoMP3 downloader allows you to free download High-quality mp3 music. The best part about this Mp3 Downloader is that you don't have to install any apps or software on your devices. </p>
+            <p>
+                SSL encryption is used by YoutoMP3 music downloader. Our site is designed to be fast, with unlimited downloads available to everyone. It gives you the option of pausing to listen to the file before downloading it. We offer separate download options for Mp3 and Mp4 formats. youtomp3 is completely free and extremely simple to use. Simply type in your search term for the songs you wish to download. </p>
+            <p>
+                The download of YoutoMP3 for Android is simple and quick. Simply type in the name of the song you wish to download. Simply follow the steps below. Choose one of the songs from the results list. Select the mp3 or mp4 file from the Download menu. After a few seconds, the mp3 download will begin automatically. The track will then be available for download and playback on your computer. </p>
+            <p>
+                YoutoMP3 is a worldwide downloader that supports 32 languages from across the world. All platforms are supported, including PC, Android, and iOS. To get started, simply open a web browser. Copy the song's URL and paste it into the youtomp3 website. After you click the search button, the YoutoMP3 download procedure will begin immediately. </p>
+            <p>
+                YoutoMP3 is a website that allows you to download songs for free. YoutoMP3, also known as YoutoMP3 cc, YoutoMP3 cc, YoutoMP3, YoutoMP3, YoutoMP3, and You to MP3, is the greatest site for downloading free MP3s. It is a completely secure and completely free online MP3 downloader. YoutoMP3 allows you to download MP3 music without worrying about limits or viruses. It is preferable to use a premium download service. However, if you want to avoid paying, YoutoMP3 could be the ideal solution, because YoutoMP3 users can acquire it for free from any website. </p>
+            <h2 align="center">Free YoutoMP3 Music Download</h2>
+            <p>
+                Type the title of the song or the URL of the music. Then press the search button. In a matter of seconds, the YoutoMP3 music downloader will display all of the available downloads. </p>
+            <p>
+                By using YoutoMP3, you agree to our Terms of Service, then select an MP3 or MP4 quality from the search results by clicking the Download button. </p>
+            <p>
+                All common web browsers, including Chrome, Firefox, Safari, Opera, and Microsoft Edge, may be used to access our free mp3 search engine website for downloading. </p>
+            <h2 align="center">YoutoMP3 Download Review</h2>
+            <p>
+                YoutoMP3 was a great tool for downloading MP3s and a free platform for finding MP3 music files across all platforms. </p>
+            <p>
+                To download your favorite MP3 music from YoutoMP3, just go to the website. You will not need to download or install any software on your PC because it is an internet service. Simply go to YoutoMP3 and download your music from there. </p>
+            <p>
+                YoutoMP3 is very simple to use; there is no need to create an account in order to download mp3 music with the YoutoMP3 - Music Downloader YoutoMP3 app. You may effortlessly download your favorite music at any moment using YoutoMP3. YoutoMP3 - free music downloader is an app for music fans who wish to listen to music in a unique way and download free music. </p>
+            <p>
+                YoutoMP3 Downloader is one of the greatest MP3 download sites for downloading free MP3 songs without having to download any software. To begin the free MP3 music download, there is no need to register or sign up. All main web browsers, including Chrome, Firefox, Safari, Opera, and Microsoft Edge, are compatible with this online music downloader. </p>
+            <p>
+                YoutoMP3 is the best music downloader available. The majority of music download providers are either ad-supported or only offer premium downloads. YoutoMP3 is a free music downloader that doesn't have any ads, is virus-free, and allows you to download MP3 music for free. It is a superb free music downloading tool for all platforms that supports both keyword and URL music downloads. </p>
+            <p>
+                Free MP3 downloading and music search. YoutoMP3 is a free music downloader that lets you search for music, listen to it in the app, and save songs for offline listening. You may find millions of your favorite songs by searching for them by title, artist, or album. </p>
+            <h2 align="center" id="use">How to use YoutoMP3?</h2>
+            <p>
+                1. Paste the URL or write some terms into the search box. </p>
+            <p>
+                2. Click the Search button. </p>
+            <p>
+                3. Wait a few seconds for the procedure to complete. </p>
+            <p>
+                4. Select the MP3/MP4 format you wish to download from the results. </p>
+            <p>
+                It simply takes 1-5 seconds to complete the search. You can then download the files you want after it's finished. A list of the mp3/mp4 results will be sent to you. It's quite simple. Click here to test YoutoMP3 right now! </p>
+            <h2 align="center">YoutoMP3 Features</h2>
+            <p>1. Unlimited download.</p>
+            <p>2. 100% safe and secure connection.</p>
+            <p>3. It's always free and simple to use.</p>
+            <p>4. High-speed download and conversion.</p>
+            <p>5. Support for audio (mp3) and video (mp4) formats.</p>
+            <p>
+                YoutoMP3 is a website that allows music fans to download mp3 files. YoutoMP3 has now assisted millions of consumers in obtaining free mp3 downloads. This is the greatest mp3 downloader available right now. Many people look for youtomp3, YoutoMP3, and YoutoMP3 on the internet. </p>
+            <p>
+                YoutoMP3 guarantees that you will never be charged, and you may get all of the music you need without having to register at youtomp3s.cc. Conclusion. YoutoMP3 is a popular web-based application that allows you to listen to and download an unlimited amount of audio songs. </p>
+            <p>
+                YoutoMP3 is a free mp3 search engine and application that allows you to download an infinite number of mp3 files from any location on the planet. You must input your question, choose the source from which you want to search, and then press the search button. You'll get a list of your results as soon as it discovers any that fit your search term. </p>
+            <h2 align="center">How to Download YoutoMP3 MP3 Music?</h2>
+            <p>
+                With only a few simple steps, you may download mp3 files. Our free mp3 search engine will return accurate results when you enter your search term in YoutoMP3's search box. You may listen to a song before downloading it using YoutoMP3's mp3 downloader. The file is available for free download in Mp3 or Mp4 formats. </p>
+            <p>
+                Go to YoutoMP3.io after you've opened your browser. To get to the search bar, go to the bottom of the page and type in "search". It's smack dab in the middle of your monitor. Put your keywords in the box. The song title of the music you wish to download may be this. You may listen to your MP3 music on any computer, tablet, or mobile device if you install YoutoMP3 on your PC. </p>
+            <p>
+                YoutoMP3 music downloader apk allows you to download an endless amount of music. Learn how to get music off the internet. YoutoMP3 is a program that allows music fans to download mp3 files. Discover how to get an app from the Windows Store. Fortunately, regardless of the technology you use, downloading apps can be quick. </p>
+            <p>
+                YoutoMP3 is a free service that does not guarantee personal or professional use. Thank you for making advantage of the YoutoMP3 download service! </p>
+        </div>
+        <div class='top'>
+            <span class='glyphicon glyphicon-menu-up'></span>
+        </div>
+    </div>
+    <div id="footer" style="margin-bottom: 20px;">
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Terms of Service</a>
+        <a href="#">Scholarship</a>
+        <a href="https://y2mate.tools/en56d8/youtube-mp3" style="display:none;">youtube to mp3</a>
+        <a href="https://y2mate.tools/en56e2/youtube-mp4" style="display:none;">youtube to mp4</a>
+        <a href="https://vvw.youtomp3.tools/" style="display:none;">youtomp3</a>
+    </div>
+    <script src="/theme/youtomp3/js/jquery.cookie.js"></script>
+    <script>
+        function download(type) {
+            var yturl = document.getElementById('query').value
+            document.getElementById('result_box').innerHTML = `
+            <iframe style="height: 50px; border:none; overflow:hidden" src="https://loader.to/api/button/?f=${type}&url=${yturl}"></iframe>
+            `;
+            document.getElementById('result_box').style.display = 'block';
+        }
+    </script>
+</body>
+
+</html>
